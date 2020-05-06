77e696201e2e7899a31c6e9e0daee57b53a4d2d5
Axicez <42653168+Axicez@users.noreply.github.com>
2020-05-06 15:35:09 +0200

    version 1000

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function klocka(){
+    let time = new Date();
+    let timme = time.getHours();
+    let minut = time.getMinutes();
+    let sekund = time.getSeconds();
+
+    if(timme < 10)
+    {
+        timme = "0" + timme;
+    }
+
+    if(minut < 10)
+    {
+        minut = "0" + minut;
+    }
+
+    if(sekund < 10)
+    {
+        sekund = "0" + sekund;
+    }
+
+    let tid = timme + ":" + minut + ":" + sekund;
+    document.getElementById("clock").innerHTML = tid;
+
+    setTimeout(klocka, 1000);
+}
+
+klocka();
